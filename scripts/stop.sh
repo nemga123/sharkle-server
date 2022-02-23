45bca71b54597069d52cdeb7d12d9641cbdee8b9
nemga123 <66145493+nemga123@users.noreply.github.com>
2022-02-24 02:50:59 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,7 @@ UWSGI_INI=/home/ec2-user/sharkle-server_uwsgi.ini
 VIRTUAL_ENV=sharkle-venv
 LOG_DIR=/home/ec2-user/sharkle_uwsgi.log
 
-ls -A
+pwd
 
 echo "[Deploy] : Activate virtual env"
 pyenv activate $VIRTUAL_ENV
