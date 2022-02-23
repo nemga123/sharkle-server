abb0ee01c7f970255e541c0ae6604a2e7c496d75
nemga123 <66145493+nemga123@users.noreply.github.com>
2022-02-24 02:26:09 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,8 +4,13 @@ UWSGI_INI=/home/ec2-user/sharkle-server_uwsgi.ini
 VIRTUAL_ENV=sharkle-venv
 LOG_DIR=/home/ec2-user/sharkle_uwsgi.log
 
+
+echo "[Deploy] : Activate virtual env"
+pyenv activate $VIRTUAL_ENV
+
 echo "[Deploy] : Stop Nginx"
 sudo systemctl stop nginx
 
 echo "[Deploy] : Reset Log"
+uwsgi --stop /tmp/sharkle_uwsgi.pid
 rm $LOG_DIR
